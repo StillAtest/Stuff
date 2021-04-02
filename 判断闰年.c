f1e503c1d5aefc7f2f6c01fcf35512a6eb0ee6cc
StillAtest <32786141+StillAtest@users.noreply.github.com>
2021-04-02 16:45:58 +0800

    Update 判断闰年.c

--- 判断闰年.c
+++ 判断闰年.c
@@ -1,14 +1,17 @@
+/*The program is used for judging whether the year is the leap year or not;
+ *StudentId:202010420227;
+ *Date:3.2 */
 #include<stdio.h>
 #include<windows.h>
 int main()
 {
     int year;
     printf("\n input the year number:\n");
-    scanf("%d",&year);
-    if((year%4==0&&year%100!=0)||(year%400==0))
-        printf("\n%d年是闰年",year);
+    scanf("%d",&year);//To get an input of the number of the year
+    if((year%4==0&&year%100!=0)||(year%400==0))//The analyzing condition of the leap year
+        printf("\n%d is the leap year",year);
     else
-        printf("\n%d年不是闰年",year);
+        printf("\n%d is not the leap year",year);//The output of getting the non-compliant number
     system("pause");
     return 0;
 }
